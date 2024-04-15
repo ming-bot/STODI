55e8a27a079476aeb644a8c41845724986d611a4
Zitong Wang <44541237+ZitWNG@users.noreply.github.com>
2024-04-15 22:03:57 +0800

    try on torque control
    try on torque control

--- franka-pybullet/STOIL/robot_model.py
+++ franka-pybullet/STOIL/robot_model.py
@@ -2,17 +2,18 @@ import numpy as np
 import time
 import pybullet as p
 
+
 class Panda:
-    def __init__(self, stepsize=1e-3, realtime=0):
+    def __init__(self, stepsize=1e-3, realtime=0) -> object:
         self.t = 0.0
         self.stepsize = stepsize
         self.realtime = realtime
 
         self.control_mode = "torque" 
 
-        self.position_control_gain_p = [0.01,0.01,0.01,0.01,0.01,0.01,0.01]
-        self.position_control_gain_d = [1.0,1.0,1.0,1.0,1.0,1.0,1.0]
-        self.max_torque = [100,100,100,100,100,100,100]
+        self.position_control_gain_p = [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01]
+        self.position_control_gain_d = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+        self.max_torque = [100, 100, 100, 100, 100, 100, 100]
 
         # connect pybullet
         p.connect(p.GUI)
@@ -22,21 +23,18 @@ class Panda:
         p.resetSimulation()
         p.setTimeStep(self.stepsize)
         p.setRealTimeSimulation(self.realtime)
-        p.setGravity(0,0,-9.81)
+        p.setGravity(0, 0, -9.81)
 
         # load models
         p.setAdditionalSearchPath("../models")
 
-        self.plane = p.loadURDF("plane/plane.urdf",
-                                useFixedBase=True)
-        p.changeDynamics(self.plane,-1,restitution=.95)
+        self.plane = p.loadURDF("plane/plane.urdf", useFixedBase=True)
+        p.changeDynamics(self.plane, -1, restitution=.95)
 
-        self.robot = p.loadURDF("panda/panda.urdf",
-                                useFixedBase=True,
-                                flags=p.URDF_USE_SELF_COLLISION)
+        self.robot = p.loadURDF("panda/panda.urdf", useFixedBase=True, flags=p.URDF_USE_SELF_COLLISION)
         
         # robot parameters
-        self.dof = p.getNumJoints(self.robot) - 1 # Virtual fixed joint between the flange and last link
+        self.dof = p.getNumJoints(self.robot) - 1   # Virtual fixed joint between the flange and last link
         if self.dof != 7:
             raise Exception('wrong urdf file: number of joints is not 7')
 
@@ -54,6 +52,8 @@ class Panda:
             self.q_max.append(joint_info[9])
             self.target_pos.append((self.q_min[j] + self.q_max[j])/2.0)
             self.target_torque.append(0.)
+            # set damping
+            p.changeDynamics(self.robot, j, linearDamping=0.1, angularDamping=100)
 
         self.reset()
 
@@ -63,7 +63,7 @@ class Panda:
         for j in range(self.dof):
             self.target_pos[j] = (self.q_min[j] + self.q_max[j])/2.0
             self.target_torque[j] = 0.
-            p.resetJointState(self.robot,j,targetValue=self.target_pos[j])
+            p.resetJointState(self.robot, j, targetValue=self.target_pos[j])
 
         self.resetController()
 
@@ -78,7 +78,7 @@ class Panda:
                                     controlMode=p.VELOCITY_CONTROL,
                                     forces=[0. for i in range(self.dof)])
 
-    def setControlMode(self, mode):
+    def setControlMode(self, mode) -> object:
         if mode == "position":
             self.control_mode = "position"
         elif mode == "velocity":
@@ -110,18 +110,20 @@ class Panda:
 
     def setTargetTorques(self, target_torque):
         self.target_torque = target_torque
-        p.setJointMotorControlArray(bodyUniqueId=self.robot,
-                                    jointIndices=self.joints,
-                                    controlMode=p.TORQUE_CONTROL,
-                                    forces=self.target_torque)
+        # 控制单个关节的运动
+        for j in range(p.getNumJoints(self.robot)):
+            p.setJointMotorControl2(self.robot, j, p.TORQUE_CONTROL, force=target_torque[j-1]*10)
+
+        # 同时控制多个关节的运动
+        # p.setJointMotorControlArray(self.robot, self.joints, p.TORQUE_CONTROL, self.target_torque)
 
     def getJointStates(self):
         joint_states = p.getJointStates(self.robot, self.joints)
         joint_pos = [x[0] for x in joint_states]
         joint_vel = [x[1] for x in joint_states]
-        return joint_pos, joint_vel 
+        return joint_pos, joint_vel
 
-    def solveInverseDynamics(self, pos, vel, acc):
+    def solveInverseDynamics(self, pos, vel, acc) -> object:
         return list(p.calculateInverseDynamics(self.robot, pos, vel, acc))
 
     def solveInverseKinematics(self, pos, ori):
@@ -169,4 +171,17 @@ class Panda:
                 self.setTargetVelocity(joints_vel_array[num, :])
                 num += 1
             self.step()
+            time.sleep(self.stepsize)
+
+    def traj_torque_control(self, joints_torque_array):
+        print(joints_torque_array)
+        duration = 256*0.025
+        num = 0
+        for i in range(int(duration/1e-3)):
+            if i % int(0.025 / 1e-3) == 0 and num < joints_torque_array.shape[0]:
+                self.setTargetTorques(joints_torque_array[num, :]*1000)
+                num += 1
+            self.step()
+            print(self.t)
+            print(self.target_torque)
             time.sleep(self.stepsize)
